a7acfb48fc87ad1dcd43c30eaa66688310cc093e
602221422 <602221422@qq.com>
2019-01-18 15:19:33 +0800

    UWP_Learn

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task t = BadAsync();
+        t.Wait();
+        Console.WriteLine("Task Status  :  {0}", t.Status);//获取任务状态
+        Console.WriteLine("Task IsFaulted  :  {0}", t.IsFaulted);//task是否由于未经处理异常的原因而完成
+    }
+    static async Task BadAsync()
+    {
+        try
+        {
+            await Task.Run(() => { throw new Exception(); });
+        }
+        catch
+        {
+            Console.WriteLine("Exception in BadAsync");
+        }
+    }
+}
+
