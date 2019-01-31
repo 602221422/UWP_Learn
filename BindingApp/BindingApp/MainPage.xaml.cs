986f8576b0063e33e2254f6d9d2be60dfd1253ff
602221422 <602221422@qq.com>
2019-01-31 17:54:59 +0800

    1.31

---
+++ BindingApp/BindingApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace BindingApp
+{
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new RecordingViewModel();
+        }
+        public RecordingViewModel ViewModel { get; set; }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var library = Windows.Storage.KnownFolders.PicturesLibrary;
+            var queryOptions = new Windows.Storage.Search.QueryOptions();
+            queryOptions.FolderDepth = Windows.Storage.Search.FolderDepth.Deep;
+            queryOptions.IndexerOption = Windows.Storage.Search.IndexerOption.UseIndexerWhenAvailable;
+
+            var fileQuery = library.CreateFileQueryWithOptions(queryOptions);
+
+            var fif = new Windows.Storage.BulkAccess.FileInformationFactory(
+                fileQuery,
+                Windows.Storage.FileProperties.ThumbnailMode.PicturesView,
+                190,
+                Windows.Storage.FileProperties.ThumbnailOptions.UseCurrentScale,
+                false
+                );
+
+            var dataSource = fif.GetVirtualizedFilesVector();
+            this.PicturesListView.ItemsSource = dataSource;
+        }
+    }
+}
---
+++ BindingApp/BindingApp/Recording.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingApp
+{
+    public class Recording
+    {
+        public string ArtistName { get; set; }
+        public string CompositionName { get; set; }
+        public DateTime ReleaseDataTime { get; set; }
+        public Recording()
+        {
+            this.ArtistName = "aaaaa";
+            this.CompositionName = "bbbb";
+            this.ReleaseDataTime = new DateTime(1761, 1, 1);
+        }
+        public string OneLineSummary
+        {
+            get
+            {
+                return $"{this.CompositionName} by {this.ArtistName},released:"
+                    + this.ReleaseDataTime.ToString("d");
+            }
+        }
+    }
+    public class RecordingViewModel
+    {
+        private Recording defaultRecording = new Recording();
+        public Recording DefaultRecording { get { return this.defaultRecording; } }
+        private ObservableCollection<Recording> recordings = new ObservableCollection<Recording>();
+        public ObservableCollection <Recording> Recordings { get { return this.recordings; } }
+        public RecordingViewModel()
+        {
+            this.recordings.Add(new Recording()
+            {
+                ArtistName = "zxcccc",
+                CompositionName = "aaaa",
+                ReleaseDataTime = new DateTime(1748, 12, 1)
+            });
+            this.recordings.Add(new Recording()
+            {
+                ArtistName = "wergsfafsd",
+                CompositionName = "wwww",
+                ReleaseDataTime = new DateTime(1748, 8, 1)
+            });
+            this.recordings.Add(new Recording()
+            {
+                ArtistName = "ttttt",
+                CompositionName = "ccccc",
+                ReleaseDataTime = new DateTime(1777, 1, 1)
+            });
+
+        }
+    }
+    public class StringFormatter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert(object value,Type targetType, object paramter,string language)
+        {
+            string formatstring = paramter as string;
+            if (!string.IsNullOrEmpty(formatstring))
+            {
+                return string.Format(formatstring, value);
+            }
+            return value.ToString();
+        }
+        public object ConvertBack(object value,Type targetType,object parameter,string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
