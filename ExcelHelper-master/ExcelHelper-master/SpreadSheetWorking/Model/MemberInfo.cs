788f3ed05ff5a800a561c38f621229e04c22afb5
602221422 <602221422@qq.com>
2019-02-15 18:01:26 +0800

    2.15

---
+++ ExcelHelper-master/ExcelHelper-master/SpreadSheetWorking/Model/MemberInfo.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadSheetWorking.Model
+{
+    public class MemberInfo : ObservableObject
+    {
+        private string username;
+        private string alias;
+        private string wsalias;
+        private string technology;
+        private string group;
+        private int vacationhour;
+        public string UserName
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+        public string Alias
+        {
+            get { return alias; }
+            set
+            {
+                alias = value;
+                RaisePropertyChanged("Alias");
+            }
+        }
+        public string WsAlias
+        {
+            get { return wsalias; }
+            set
+            {
+                wsalias = value;
+                RaisePropertyChanged("WsAlias");
+            }
+        }
+        public string Technology
+        {
+            get { return technology; }
+            set
+            {
+                technology = value;
+                RaisePropertyChanged("Technology");
+            }
+        }
+        public string Group
+        {
+            get { return group; }
+            set
+            {
+                group = value;
+                RaisePropertyChanged("Group");
+            }
+        }
+
+        public int VacationHour
+        {
+            get { return vacationhour; }
+            set
+            {
+                vacationhour = value;
+                RaisePropertyChanged("VacationHour");
+            }
+        }
+        /*public static ObservableCollection<MemberInfo> GetMemberInfos()
+        {
+            return new ObservableCollection<MemberInfo>()
+            {
+                new MemberInfo()
+                {
+                    UserName="saaa",
+                    Alias="aaa",
+                    wsalias="ddd",
+                    Technology="ooo",
+                    Group="ssss",
+                    VacationHour=6,
+                },
+            };
+        }*/
+
+        public void emptymem()
+        {
+            username = null;
+            alias = null;
+            wsalias = null;
+            technology = null;
+            group = null;
+            vacationhour = 0;
+        }
+    }
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ExcelHelper-master/ExcelHelper-master/SpreadSheetWorking/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using SpreadSheetWorking.Model;
+using SpreadSheetWorking;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+namespace SpreadSheetWorking.ViewModel
+{
+    class MainPageViewModel:ObservableObject
+    {
+        private ObservableCollection<MemberInfo> members;
+        public ObservableCollection<MemberInfo> Members
+        {
+            get { return members; }
+            set
+            {
+                members = value;
+                RaisePropertyChanged("Members");
+            }
+        }
+        public MainPageViewModel()
+        {
+            Readfile();
+        }
+        async public void Readfile()
+        {
+            Stream finalstream = await SpreadsheetHelper.filepathhelper();
+            try
+            {
+                SpreadsheetHelper.ReadDataFromExcel(finalstream, "Sheet1", "A2", "F29", members);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+}
