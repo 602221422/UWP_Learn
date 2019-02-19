4a4f76005c8057293857833b7da0095e62f97594
602221422 <602221422@qq.com>
2019-02-19 17:55:56 +0800

    2.19

---
+++ TouchApp/TouchApp/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace TouchApp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private TranslateTransform dragTranslation;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            show.PointerPressed += TouchRectangle_PointerPressed;
+            show.PointerReleased += TouchRectangle_PointerReleased;
+            show.PointerExited += Touch_PinterExited;
+            show.ManipulationDelta += touchRectangle_ManipulationDelta;
+            dragTranslation = new TranslateTransform();
+            show.RenderTransform = this.dragTranslation;
+
+        }
+        private void Touch_PinterExited(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+            if (null != rect)
+            {
+                rect.Width = 200;
+                rect.Height = 100;
+            }
+        }
+        private void TouchRectangle_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+            if (null != rect)
+            {
+                rect.Width = 200;
+                rect.Height = 100;
+            }
+        }
+        private void TouchRectangle_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+            if (null != rect)
+            {
+                rect.Width = 250;
+                rect.Height = 150;
+            }
+        }
+        void touchRectangle_ManipulationDelta(object sender,ManipulationDeltaRoutedEventArgs e)
+        {
+            // Move the rectangle.
+            dragTranslation.X += e.Delta.Translation.X;
+            dragTranslation.Y += e.Delta.Translation.Y;
+        }
+
+    }
+}
